9ab0f807169b1fce77f3933b0776286e5c73fc64
ahmed alshmre <ahmedalshmre06@outlook.com>
2020-12-02 21:53:27 +0300

    first commit

---
+++ stripe_payment/lib/constants.dart
@@ -0,0 +1,6 @@
+///These keys are revoked. Use keys and product ids from Stripe dashboard.
+const apiKey =
+    '	pk_test_ikolKNDPCcxdpRnFPWy5dl0B00PA8Eri4X';
+const secretKey =
+    'sk_test_4QswxseX61IxdMUz0Vxrzh0N00zSg2dQ2F';
+const nikesPriceId = 'price_1Htz4nEnajNI5XqMkznNlr7B';
